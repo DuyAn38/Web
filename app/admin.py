22c7d01d7ab465326909711c7767a9e2ecde9181
DuyAn38 <duyanpx2001@gmail.com>
2023-09-07 10:11:46 +0700

    fix add product button

---
+++ app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Comment)
+admin.site.register(Slide)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Adress)
+admin.site.register(Cart)
+
+# Register your models here.
---
+++ app/views.py
@@ -0,0 +1,36 @@
+# from itertools import product
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import *
+# app
+from .python.app.base import *
+from .python.app.information_address import information_address
+from .python.app.cart import cart
+
+from .python.app.category import category
+from .python.app.check_address import Continue1
+from .python.app.checkout import checkout
+from .python.app.detail import detail
+from .python.app.information import Information
+from .python.app.login import loginPage, logoutPage
+from .python.app.register import register
+from .python.app.search import searchProduct
+from .python.app.updateItem import updateItem
+
+from .python.app.manage_address import addAddress, editAddress, deleteAddress
+# admin
+from .python.admin.manage import Manage
+from .python.admin.manage_slide import manageSlide
+from .python.admin.manage_user import manageUser
+from .python.admin.manage_category import manageCategory, addCategory, editCategory, deleteCategory
+from .python.admin.manage_product import manageProduct, addProduct, editProduct, deleteProduct, viewProduct
+#API
+from .API.products_api import *
+from .API.category_api import *
+
+
+
+
+
+
